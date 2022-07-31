e2e8ecf49770c41c323a7492397dab05ab59253e
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-07-31 14:55:22 -0400

    configure for daisyui

--- mb_jth3/src/App.jsx
+++ mb_jth3/src/App.jsx
@@ -5,8 +5,13 @@ import './App.css'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <>
+       <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+      </h1>
+      <button className='btn btn-primary px-16 m-10 py-4 rounded-full'>Click</button>
+    </>
+   
+   
   )
 }
